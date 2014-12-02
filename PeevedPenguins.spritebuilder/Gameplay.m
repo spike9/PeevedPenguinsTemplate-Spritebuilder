1ccdb9e1eda262e99c921c794bf11aca459ee2c1
spike9 <d.wyss@gmx.ch>
2014-12-02 17:46:04 +0100

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.m
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -27,7 +27,7 @@
     // Loads the Penguin.cbb we have set up in Spritebuilder
     CCNode *penguin = [CCBReader load:@"Penguin"];
     // position the penguin at the bowl of the catapult
-    penguin.position = ccpAdd(_catapultArm.position, ccp(16, 130));
+    penguin.position = ccpAdd(_catapultArm.position, ccp(20, 135));
     
     // add the penguin to the physicsNode of this scene (because it has physics enabled)
     [_physicsNode addChild:penguin];
